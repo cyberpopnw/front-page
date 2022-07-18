eed411a0f2606a6c3a1502c36fc6205e5781028a
tonori <tonori@tonori.cn>
2022-07-18 15:13:29 +0800

    feat: Partner gate.io

--- src/views/home/pages/HomeB.vue
+++ src/views/home/pages/HomeB.vue
@@ -371,100 +371,7 @@
       </ul>
     </div>
   </div>
-  <div class="partners">
-    <div class="title flex_center" id="ele6">{{ $t('message.home.part_title') }}
-      <!--            <img class="xplan" @click="showxplan()" src="@/assets/nwhome/xplan.svg" alt="" >-->
-    </div>
-    <div class="logo flex_between">
-      <a href="https://www.ubisoft.com/en-us/" target="view_window">
-        <img class="logo9" src="@/assets/nwhome/UBISOFT.png" alt="">
-      </a>
-      <a href="https://aws.amazon.com/" target="view_window">
-        <img class="logo2" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/aws.png" alt="">
-      </a>
-      <a href="https://www.avax.network/" target="view_window">
-        <img class="logo8" src="@/assets/nwhome/make-avala.svg" alt="">
-      </a>
-      <a href="https://www.mexc.com/pioneer" target="view_window">
-        <img class="logo12" src="@/assets/nwhome/pioneerlogo.png" alt="">
-      </a>
-      <a href="https://www.mexc.com/" target="view_window">
-        <img class="logo12" src="@/assets/nwhome/mexc.png" alt="">
-      </a>
-      <a href="https://www.gate.io" target="view_window">
-        <img class="logo10" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_gate.png" alt="">
-      </a>
-      <div>
-        <img class="logo8" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/hashshareBank.png" alt="">
-      </div>
-      <a href="https://consensys.net/" target="view_window">
-        <img class="logo7" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/consensys-logo.svg" alt="">
-      </a>
-      <div>
-        <img class="logo1" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/partners4.png" alt="">
-      </div>
-      <a href="https://skywater.vc/" target="view_window">
-        <img class="logo3" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/skywater2.png" alt="">
-      </a>
-      <a href="https://www.jinance.org/" target="view_window">
-        <img class="logo4" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/partners3.png" alt="">
-      </a>
-      <a href="https://t3e.vc/" target="view_window">
-        <img class="logo5" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/T3E-Logo.png" alt="">
-      </a>
-      <a href="https://polygon.technology/" target="view_window">
-        <img class="logo12" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_polygon.png" alt="">
-      </a>
-      <div>
-        <img class="logo6" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/partners5-2.png" alt="">
-      </div>
-      <a href="http://www.abga.asia/" target="view_window">
-        <img class="logo10" src="@/assets/nwhome/ABGA.svg" alt="">
-      </a>
-      <a href="https://www.earnguild.io/" target="view_window">
-        <img class="logo11" src="@/assets/nwhome/Earn-Guild.png" alt="">
-      </a>
-      <!-- <a href="https://ethereum.org/en/" target="view_window">
-          <img class="logo12" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_eth.png" alt="">
-      </a> -->
-      <!-- <a href="https://www.binance.com/" target="view_window">
-          <img class="logo12" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_binan.png" alt="">
-      </a> -->
-      <a href="https://pancakeswap.finance/" target="view_window">
-        <img class="logo12" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_pancake.png" alt="">
-      </a>
-      <a href="https://metamask.io/" target="view_window">
-        <img class="logo12" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_metamask.png" alt="">
-      </a>
-      <a href="https://www.tokenpocket.pro/" target="view_window">
-        <img class="logo12" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_tokenpocket.png" alt="">
-      </a>
-      <a href="https://opensea.io/" target="view_window">
-        <img class="logo12" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_opensea.png" alt="">
-      </a>
-      <a href="https://tofunft.com/" target="view_window">
-        <img class="logo11" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_tofu.png" alt="">
-      </a>
-      <a href="https://chain.link/" target="view_window">
-        <img class="logo12" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/chain_link.png" alt="">
-      </a>
-      <!-- <a href="https://www.bybit.com/en-US/" target="view_window">
-          <img class="logo10" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_bybit.png" alt="">
-      </a> -->
-      <div>
-        <img class="logo12" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_jdventures.png" alt="">
-      </div>
-      <a href="https://www.nsguild.net/guild" target="view_window">
-        <img class="logo12" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_nsguild.png" alt="">
-      </a>
-      <div>
-        <img class="logo11" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_richmeta.png" alt="">
-      </div>
-      <a href="https://coinmarketcap.com/" target="view_window">
-        <img class="logo12" src="https://d2cimmz3cflrbm.cloudfront.net/nwhome/alpha_coinMarkrt.png" alt="">
-      </a>
-    </div>
-  </div>
+  <Partners />
   <div class="will">
     <div class="title" id="ele6">{{ $t('message.home.will_title') }}</div>
     <ul class="logo flex_centent_center">
@@ -514,6 +421,9 @@ SwiperCore.use([EffectFade, EffectCreative, Mousewheel, Autoplay]);
 
 import { gateAuthorization, getUserInfo } from '@/api/gateOauth'
 import { clearToken, goToAuthorization, isGateClient } from '@/tools/gateIo'
+
+// Section
+import Partners from '@/views/home/section/mobile/Partners.vue'
 
 const { t } = useI18n();
 const { proxy } = getCurrentInstance() as any;
@@ -2426,104 +2336,6 @@ onMounted(() => {
   }
 }
 
-.partners {
-  width: 100%;
-  height: 1340px;
-  margin: -2px 0;
-  margin-top: -60px;
-  background-color: #000000;
-  overflow: hidden;
-
-  .title {
-    height: 31px;
-    margin-bottom: 36px;
-    font-size: 22px;
-    font-family: AlibabaPuHuiTi_2_115_Black;
-    color: #FFFFFF;
-    line-height: 28px;
-    text-align: center;
-
-    .xplan {
-      width: 38px;
-      margin-left: 12px;
-    }
-
-    .xplan:hover {
-      filter: drop-shadow(0 0 .6vw #776fe5);
-    }
-  }
-
-  .logo {
-    flex-wrap: wrap;
-    width: 100%;
-    padding: 0 38px;
-
-    a, div {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 140px;
-      // height: 87.5px;
-      height: 68px;
-      margin-bottom: 20px;
-      background: #282626;
-      border-radius: 7px;
-
-      .logo1 {
-        height: 50px;
-      }
-
-      .logo2 {
-        height: 40px;
-      }
-
-      .logo3 {
-        width: 120px;
-        margin-right: 6px;
-      }
-
-      .logo4 {
-        width: 80px;
-      }
-
-      .logo5 {
-        width: 84px;
-      }
-
-      .logo6 {
-        height: 54px;
-      }
-
-      .logo7 {
-        height: 48px;
-      }
-
-      .logo8 {
-        width: 110px;
-      }
-
-      .logo9 {
-        width: 86px;
-      }
-
-      .logo10 {
-        width: 82px;
-      }
-
-      .logo11 {
-        height: 44px;
-      }
-
-      .logo12 {
-        width: 110px;
-      }
-    }
-
-    a:hover, div:hover {
-      background: rgba(40, 38, 38, .6);
-    }
-  }
-}
 
 .will {
   width: 100%;
